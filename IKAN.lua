427470f331fc2c056a730aad2c26c5ba864c0d66
MROCTOPUS456 <167027091+MROCTOPUS456@users.noreply.github.com>
2026-02-26 00:26:03 +0700

    Update IKAN.lua

--- IKAN.lua
+++ IKAN.lua
@@ -1,6 +1,6 @@
 _G.FishItConfig = _G.FishItConfig or {
     ["Fishing"] = {
-        ["Auto Perfect"] = true,
+        ["Auto Perfect"] = false,
         ["Random Result"] = false,
 
         ["Auto Favorite"] = true,
@@ -148,6 +148,7 @@ _G.FishItConfig = _G.FishItConfig or {
     ["HideGUI"] = false,
     ["EXIT_MAP_IF_DISCONNECT"] = true,
 }
+
 
 
 
