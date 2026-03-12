4ea256b71b925e5c3baca3c67e459d33ef79e3f6
MROCTOPUS456 <167027091+MROCTOPUS456@users.noreply.github.com>
2026-03-12 23:59:37 +0700

    Update IKAN.lua

--- IKAN.lua
+++ IKAN.lua
@@ -139,7 +139,7 @@ _G.FishItConfig = _G.FishItConfig or {
         ["Endgame"] = "",
     },
 
-    ["ExtremeFpsBoost"] = false,
+    ["ExtremeFpsBoost"] = true,
     ["UltimatePerformance"] = false,
     ["Disable3DRender"] = false,
     ["AutoRemovePlayer"] = true,
@@ -148,6 +148,7 @@ _G.FishItConfig = _G.FishItConfig or {
     ["HideGUI"] = true,
     ["EXIT_MAP_IF_DISCONNECT"] = true,
 }
+
 
 
 
